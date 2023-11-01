a711503d17a517b7bde93a0c296bffc098beafb5
RootOfMinus1ATU <s00240122@atu.ie>
2023-11-01 14:25:46 +0000

    issues progress

--- next/mosh-issue-tracker/app/NavBar.tsx
+++ next/mosh-issue-tracker/app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+  const currentPath = usePathname()
+
   const links = [
     {
       label: 'Dashboard',
@@ -20,9 +26,15 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {
           links.map((link) => (
-            <li><Link 
+            <li key={link.label}><Link 
               href={link.href}
-              className='text-zinc-500 hover:text-zinc-800 transition-colors'
+              className={
+                classnames({
+                  'text-zinc-900': link.href === currentPath,
+                  'text-zinc-500': link.href !== currentPath,
+                  'hover:text-zinc-800 transition-colors': true
+                })
+              }
             >
               {link.label}
             </Link></li>
---
+++ next/mosh-issue-tracker/app/issues/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      IssuesPage
+      <Button>
+        <Link href='/issues/new'>New Issue</Link>
+      </Button>
+    </div>
+  )
+}
+
+export default IssuesPage
